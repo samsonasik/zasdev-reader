e3c5b49c1bfb07876480f897f342768b0a0ce675
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-05-30 20:44:21 +0200

    Created Role entity

---
+++ module/Application/src/Application/Entity/Role.php
@@ -0,0 +1,69 @@
+<?php
+namespace Application\Entity;
+
+use ZasDev\Common\Entity\AbstractEntity;
+use Doctrine\ORM;
+
+/**
+ * Role entity
+ * @author ZasDev
+ * @link https://github.com/zasDev
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="roles")
+ */
+class Role extends AbstractEntity
+{
+
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    private $name;
+
+    /**
+     * @param $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param $name
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+} 
