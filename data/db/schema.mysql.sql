1c7188e61a48754d4b310bbd0b80e1eab51ec64d
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-05-30 19:05:50 +0200

    Added sessions table to mysql database schema

--- data/db/schema.mysql.sql
+++ data/db/schema.mysql.sql
@@ -18,5 +18,17 @@ CREATE TABLE `users` (
   UNIQUE KEY `uuid_UNIQUE` (`uuid`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `fk_users_1_idx` (`role_id`),
-  CONSTRAINT `fk_users_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+  CONSTRAINT `fk_users_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci;
+
+CREATE TABLE `sessions` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `token` VARCHAR(256) NOT NULL,
+  `valid` TINYINT(1) NOT NULL DEFAULT 1,
+  `expiration_date` DATETIME NOT NULL,
+  `ip_address` VARCHAR(50) NULL,
+  `user_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_sessions_1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_sessions_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
