d2f6b41a8a1e67a2c63536ff4b5259a0c180980d
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-05-30 20:11:27 +0200

    Added comments table to mysql database schema

--- data/db/schema.mysql.sql
+++ data/db/schema.mysql.sql
@@ -143,3 +143,17 @@ CREATE TABLE `shared_feeds` (
   CONSTRAINT `fk_shared_feeds_1` FOREIGN KEY (`feed_id`) REFERENCES `feeds` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_shared_feeds_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci;
+
+CREATE TABLE `comments` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `body` TEXT NULL,
+  `user` VARCHAR(512) NULL,
+  `url` VARCHAR(1024) NULL,
+  `feed_id` INT NOT NULL,
+  `parent_id` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_comments_1_idx` (`feed_id` ASC),
+  INDEX `fk_comments_2_idx` (`parent_id` ASC),
+  CONSTRAINT `fk_comments_1` FOREIGN KEY (`feed_id`) REFERENCES `feeds` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_comments_2` FOREIGN KEY (`parent_id`) REFERENCES `comments` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+);
