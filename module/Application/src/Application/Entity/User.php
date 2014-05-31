2cec071c235a99d5cbfce334fe6b1104e0a7e33d
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-05-31 12:30:57 +0200

    Created User entity

---
+++ module/Application/src/Application/Entity/User.php
@@ -0,0 +1,214 @@
+<?php
+namespace Application\Entity;
+
+use ZasDev\Common\Entity\AbstractEntity;
+
+/**
+ * User entity
+ * @author ZasDev
+ * @link https://github.com/zasDev
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="users")
+ */
+class User extends AbstractEntity
+{
+
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    private $name;
+    /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    private $email;
+    /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    private $uuid;
+    /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    private $username;
+    /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    private $password;
+    /**
+     * @var bool
+     *
+     * @ORM\Column(type="boolean")
+     */
+    private $enabled;
+    /**
+     * @var Role
+     *
+     * @ORM\ManyToOne(targetEntity="Application\Entity\Role")
+     */
+    private $role;
+
+    /**
+     * @param string $email
+     * @return $this
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param boolean $enabled
+     * @return $this
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @param int $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $password
+     * @return $this
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param \Application\Entity\Role $role
+     * @return $this
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+        return $this;
+    }
+
+    /**
+     * @return \Application\Entity\Role
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param string $username
+     * @return $this
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $uuid
+     * @return $this
+     */
+    public function setUuid($uuid)
+    {
+        $this->uuid = $uuid;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+
+
+} 
