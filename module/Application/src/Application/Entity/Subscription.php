62d78dd7ebaa5b14c862b1c27814b584a1294dae
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-06-01 22:04:05 +0200

    Created Subscription entity

---
+++ module/Application/src/Application/Entity/Subscription.php
@@ -0,0 +1,173 @@
+<?php
+namespace Application\Entity;
+
+use ZasDev\Common\Entity\AbstractEntity;
+
+/**
+ * Subscription entity
+ * @author ZasDev
+ * @link https://github.com/zasDev
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="subscriptions")
+ */
+class Subscription extends AbstractEntity
+{
+
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    private $name;
+    /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    private $url;
+    /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    private $favicon;
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="Application\Entity\User")
+     */
+    private $user;
+    /**
+     * @var FeedFolder
+     *
+     * @ORM\ManyToOne(targetEntity="Application\Entity\FeedFolder")
+     */
+    private $folder;
+
+    /**
+     * @param mixed $favicon
+     * @return $this;
+     */
+    public function setFavicon($favicon)
+    {
+        $this->favicon = $favicon;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFavicon()
+    {
+        return $this->favicon;
+    }
+
+    /**
+     * @param mixed $folder
+     * @return $this;
+     */
+    public function setFolder($folder)
+    {
+        $this->folder = $folder;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFolder()
+    {
+        return $this->folder;
+    }
+
+    /**
+     * Tells if this subscription has to be placed in the root
+     * @return bool
+     */
+    public function hasFolder()
+    {
+        return !is_null($this->folder);
+    }
+
+    /**
+     * @param mixed $id
+     * @return $this;
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $name
+     * @return $this;
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $url
+     * @return $this;
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param mixed $user
+     * @return $this;
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+} 
