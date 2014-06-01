15c4bfeea78e7162c03300e52f0f8d89f3d26e8e
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-06-01 21:26:21 +0200

    Created BookmarkCategory entity

---
+++ module/Application/src/Application/Entity/BookmarkCategory.php
@@ -0,0 +1,116 @@
+<?php
+namespace Application\Entity;
+
+use ZasDev\Common\Entity\AbstractEntity;
+
+/**
+ * Class BookmarkCategory
+ * @author ZasDev
+ * @link https://github.com/zasDev
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="bookmark_categories")
+ */
+class BookmarkCategory extends AbstractEntity
+{
+
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    private $name;
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="Application\Entity\User")
+     */
+    private $user;
+    /**
+     * @var BookmarkCategory
+     *
+     * @ORM\ManyToOne(targetEntity="Application\Entity\BookmarkCategory")
+     */
+    private $parent;
+
+    /**
+     * @param int $id
+     * @return $this;
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $name
+     * @return $this;
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param \Application\Entity\BookmarkCategory $parent
+     * @return $this;
+     */
+    public function setParent($parent)
+    {
+        $this->parent = $parent;
+        return $this;
+    }
+
+    /**
+     * @return \Application\Entity\BookmarkCategory
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * @param \Application\Entity\User $user
+     * @return $this;
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * @return \Application\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+}
