c980caa9f9615c2c1aa0ffb93283a49bbea4b768
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-06-02 10:17:06 +0200

    Added en and es language files and translator config to module

---
+++ module/ZasDev/Common/config/module.config.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+
+    'translator' => array(
+        'translation_file_patterns' => array(
+            array(
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ),
+        ),
+    ),
+
+);
