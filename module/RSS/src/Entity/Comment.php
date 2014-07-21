a27b1d93d00f41f213811f9a3ee7a7061f507dd1
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-07-21 20:06:23 +0200

    Moved RSS-related entities to RSS module and updated configuration

--- module/RSS/src/Entity/Comment.php
+++ module/RSS/src/Entity/Comment.php
@@ -1,18 +1,19 @@
 <?php
-namespace Application\Entity;
+namespace RSS\Entity;
 
+use Application\Entity\User;
 use ZasDev\Common\Entity\AbstractEntity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * SharedFeed entity
+ * Comment entity
  * @author ZasDev
  * @link https://github.com/zasDev
  *
  * @ORM\Entity()
- * @ORM\Table(name="shared_feeds")
+ * @ORM\Table(name="comments")
  */
-class SharedFeed extends AbstractEntity
+class Comment extends AbstractEntity
 {
     /**
      * @var int
@@ -27,19 +28,49 @@ class SharedFeed extends AbstractEntity
      *
      * @ORM\Column()
      */
-    private $publicUrl;
+    private $body;
+    /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    private $url;
     /**
      * @var Feed
      *
-     * @ORM\ManyToOne(targetEntity="Application\Entity\Feed")
+     * @ORM\ManyToOne(targetEntity="RSS\Entity\Feed")
      */
     private $feed;
+    /**
+     * @var Comment
+     *
+     * @ORM\ManyToOne(targetEntity="RSS\Entity\Comment")
+     */
+    private $parent;
     /**
      * @var User
      *
      * @ORM\ManyToOne(targetEntity="Application\Entity\User")
      */
     private $user;
+
+    /**
+     * @param string $body
+     * @return $this;
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
 
     /**
      * @param \Application\Entity\Feed $feed
@@ -78,26 +109,53 @@ class SharedFeed extends AbstractEntity
     }
 
     /**
-     * @param string $publicUrl
+     * @param \Application\Entity\Comment $parent
      * @return $this;
      */
-    public function setPublicUrl($publicUrl)
+    public function setParent($parent)
     {
-        $this->publicUrl = $publicUrl;
+        $this->parent = $parent;
+        return $this;
+    }
+
+    /**
+     * @return \Application\Entity\Comment
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * Tells if this comment has a parent comment
+     * @return bool
+     */
+    public function hasParent()
+    {
+        return !is_null($this->parent);
+    }
+
+    /**
+     * @param string $url
+     * @return $this;
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
         return $this;
     }
 
     /**
      * @return string
      */
-    public function getPublicUrl()
+    public function getUrl()
     {
-        return $this->publicUrl;
+        return $this->url;
     }
 
     /**
      * @param \Application\Entity\User $user
-     * @return $this;
+     * @return $this
      */
     public function setUser($user)
     {
--- module/RSS/src/Entity/Subscription.php
+++ module/RSS/src/Entity/Subscription.php
@@ -1,18 +1,19 @@
 <?php
-namespace Application\Entity;
+namespace RSS\Entity;
 
+use Application\Entity\User;
 use ZasDev\Common\Entity\AbstractEntity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * FeedFolder entity
+ * Subscription entity
  * @author ZasDev
  * @link https://github.com/zasDev
  *
  * @ORM\Entity()
- * @ORM\Table(name="feed_folders")
+ * @ORM\Table(name="subscriptions")
  */
-class FeedFolder extends AbstractEntity
+class Subscription extends AbstractEntity
 {
     /**
      * @var int
@@ -29,6 +30,18 @@ class FeedFolder extends AbstractEntity
      */
     private $name;
     /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    private $url;
+    /**
+     * @var string
+     *
+     * @ORM\Column()
+     */
+    private $favicon;
+    /**
      * @var User
      *
      * @ORM\ManyToOne(targetEntity="Application\Entity\User")
@@ -37,9 +50,54 @@ class FeedFolder extends AbstractEntity
     /**
      * @var FeedFolder
      *
-     * @ORM\ManyToOne(targetEntity="Application\Entity\FeedFolder")
+     * @ORM\ManyToOne(targetEntity="RSS\Entity\FeedFolder")
      */
-    private $parent;
+    private $folder;
+
+    /**
+     * @param string $favicon
+     * @return $this;
+     */
+    public function setFavicon($favicon)
+    {
+        $this->favicon = $favicon;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFavicon()
+    {
+        return $this->favicon;
+    }
+
+    /**
+     * @param FeedFolder $folder
+     * @return $this;
+     */
+    public function setFolder($folder)
+    {
+        $this->folder = $folder;
+        return $this;
+    }
+
+    /**
+     * @return FeedFolder
+     */
+    public function getFolder()
+    {
+        return $this->folder;
+    }
+
+    /**
+     * Tells if this subscription has to be placed in the root
+     * @return bool
+     */
+    public function hasFolder()
+    {
+        return !is_null($this->folder);
+    }
 
     /**
      * @param int $id
@@ -78,34 +136,25 @@ class FeedFolder extends AbstractEntity
     }
 
     /**
-     * @param FeedFolder $parent
+     * @param string $url
      * @return $this;
      */
-    public function setParent($parent)
+    public function setUrl($url)
     {
-        $this->parent = $parent;
+        $this->url = $url;
         return $this;
     }
 
     /**
-     * @return FeedFolder
+     * @return string
      */
-    public function getParent()
+    public function getUrl()
     {
-        return $this->parent;
+        return $this->url;
     }
 
     /**
-     * Tells if this FeedFolder has a parent folder
-     * @return bool
-     */
-    public function hasParent()
-    {
-        return !is_null($this->parent);
-    }
-
-    /**
-     * @param \Application\Entity\User $user
+     * @param User $user
      * @return $this;
      */
     public function setUser($user)
@@ -115,7 +164,7 @@ class FeedFolder extends AbstractEntity
     }
 
     /**
-     * @return \Application\Entity\User
+     * @return User
      */
     public function getUser()
     {
