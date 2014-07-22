97604dab6c2a27e04f377bb4d3f75605f7c33088
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-07-22 20:53:56 +0200

    Performed some updates on Auth module to avoid it to interfere with console controllers

--- module/Auth/src/Module.php
+++ module/Auth/src/Module.php
@@ -2,17 +2,14 @@
 namespace Auth;
 
 use Auth\Service\AuthCheckerService;
-use Auth\Service\AuthCheckerServiceInterface;
-use Zend\Debug\Debug;
 use Zend\EventManager\EventInterface;
 use Zend\Http\Response;
 use Zend\Loader\StandardAutoloader;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\BootstrapListenerInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
-use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
-use Zend\ServiceManager\ServiceManager;
+use Zend\Http\Response as HttpResponse;
 
 /**
  * Class Module
@@ -57,11 +54,12 @@ class Module implements
             $service = $event->getApplication()->getServiceManager()->get('Auth\Service\AuthCheckerService');
             $service->setEvent($event);
             if (!$service->checkAuthentication()) {
-                /* @var Response $resp */
                 $resp = $event->getResponse();
-                $resp->setStatusCode(302)
-                     ->getHeaders()->addHeaders(array("Location" => "/login"));
-                return $resp;
+                if ($resp instanceof HttpResponse) {
+                    $resp->setStatusCode(302)
+                         ->getHeaders()->addHeaders(array("Location" => "/login"));
+                    return $resp;
+                }
             }
         }, 100);
     }
