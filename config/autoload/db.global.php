97f48c8f8671f27952861b8b5b4c160746e1dde2
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-07-27 12:40:52 +0200

    Updated db config files, removing from global file all the content that will be dynamically created by the user on installation

--- config/autoload/db.global.php
+++ config/autoload/db.global.php
@@ -39,17 +39,8 @@ return array(
         // Database connection params
         'connection' => array(
             'orm_default' => array(
-                'driverClass' =>'Doctrine\DBAL\Driver\PDOMySql\Driver',
-                'params' => array(
-                    'host'          => 'localhost',
-                    'port'          => '3306',
-                    'user'          => '',
-                    'password'      => '',
-                    'dbname'        => '',
-                    'driverOptions' => array(
-                        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-                    ),
-                )
+//                'driverClass' =>'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array()
             )
         ),
         // Authentication service
