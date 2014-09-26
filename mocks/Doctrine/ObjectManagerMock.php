88395abec89635db0eb5dc174614bdd328d29084
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-09-26 20:59:34 +0200

    Created folder to store shared module mocks

---
+++ mocks/Doctrine/ObjectManagerMock.php
@@ -0,0 +1,208 @@
+<?php
+namespace ZasDev\Mock\Doctrine;
+
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ObjectManagerMock implements ObjectManager
+{
+    const DEFAULT_REPO_CLASS_NAME = 'ZasDev\Mock\Doctrine\ObjectRepositoryMock';
+
+    /**
+     * @var array
+     */
+    protected $cache;
+    /**
+     * Maps entities with repository mocks
+     * @var array
+     */
+    protected $repositories;
+    /**
+     * @var bool
+     */
+    protected $flushed = false;
+
+    public function __construct(array $entityRepositoryMap)
+    {
+        $this->cache        = array();
+        $this->repositories = $entityRepositoryMap;
+    }
+
+    /**
+     * @param string $className The class name of the object to find.
+     * @param mixed $id The identity of the object to find.
+     * @return object The found object.
+     */
+    public function find($className, $id)
+    {
+        $repo = $this->getRepository($className);
+        return $repo->find($id);
+    }
+
+    /**
+     * @param object $object The instance to make managed and persistent.
+     * @return void
+     */
+    public function persist($object)
+    {
+        $hash                           = spl_object_hash($object);
+        $className                      = get_class($object);
+        $this->cache[$className][$hash] = $object;
+
+        // If the object has an ID property and it's not been set, set a random value
+        if (method_exists($object, 'getId')) {
+            $oldId = $object->getId();
+            if (empty($oldId) && method_exists($object, 'setId')) {
+                $object->setId(rand(9999, 99999999)); // Probably this number won't be used in a test
+            }
+        }
+    }
+
+    /**
+     * @param object $object The object instance to remove.
+     * @return void
+     */
+    public function remove($object)
+    {
+        $hash       = spl_object_hash($object);
+        $className  = get_class($object);
+        unset($this->cache[$className][$hash]);
+    }
+
+    /**
+     * @param object $object
+     * @return object
+     */
+    public function merge($object)
+    {
+        $cloned = clone $object;
+        $this->persist($cloned);
+        return $cloned;
+    }
+
+    /**
+     * @param string|null $objectName if given, only objects of this type will get detached.
+     * @return void
+     */
+    public function clear($objectName = null)
+    {
+        $this->cache = array();
+    }
+
+    /**
+     * @param object $object The object to detach.
+     * @return void
+     */
+    public function detach($object)
+    {
+        $this->remove($object);
+    }
+
+    /**
+     * @param object $object The object to refresh.
+     * @return void
+     */
+    public function refresh($object)
+    {
+        $hash       = spl_object_hash($object);
+        $className  = get_class($object);
+        if (array_key_exists($className, $this->cache) && array_key_exists($hash, $this->cache[$className])) {
+            $object = $this->cache[$hash];
+        }
+    }
+
+    /**
+     * @param object $object
+     * @return bool
+     */
+    public function contains($object)
+    {
+        $hash = spl_object_hash($object);
+        foreach ($this->cache as $repository) {
+            if (is_array($repository) && array_key_exists($hash, $repository)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @return void
+     */
+    public function flush()
+    {
+        $this->flushed = true;
+    }
+
+    /**
+     * @param string $className
+     * @return \Doctrine\Common\Persistence\ObjectRepository
+     */
+    public function getRepository($className)
+    {
+        $repositoryName = (!array_key_exists($className, $this->repositories))
+            ? self::DEFAULT_REPO_CLASS_NAME
+            : $this->repositories[$className];
+        return new $repositoryName(
+            $this,
+            $className,
+            isset($this->cache[$className])? $this->cache[$className] : array()
+        );
+    }
+
+    /*
+     * CUSTOM METHODS
+     */
+
+    public function getCache()
+    {
+        return $this->cache;
+    }
+    public function isFlushed()
+    {
+        return $this->flushed;
+    }
+
+    /*
+     * METHODS NOT YET IMPLEMENTED
+     */
+
+    /**
+     * Returns the ClassMetadata descriptor for a class.
+     *
+     * The class name must be the fully-qualified class name without a leading backslash
+     * (as it is returned by get_class($obj)).
+     *
+     * @param string $className
+     *
+     * @return \Doctrine\Common\Persistence\Mapping\ClassMetadata
+     */
+    public function getClassMetadata($className)
+    {
+        // TODO: Implement getClassMetadata() method.
+    }
+
+    /**
+     * Gets the metadata factory used to gather the metadata of classes.
+     *
+     * @return \Doctrine\Common\Persistence\Mapping\ClassMetadataFactory
+     */
+    public function getMetadataFactory()
+    {
+        // TODO: Implement getMetadataFactory() method.
+    }
+
+    /**
+     * Helper method to initialize a lazy loading proxy or persistent collection.
+     *
+     * This method is a no-op for other objects.
+     *
+     * @param object $obj
+     *
+     * @return void
+     */
+    public function initializeObject($obj)
+    {
+        // TODO: Implement initializeObject() method.
+    }
+}
---
+++ mocks/Doctrine/ObjectRepositoryMock.php
@@ -0,0 +1,202 @@
+<?php
+namespace ZasDev\Mock\Doctrine;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+
+class ObjectRepositoryMock implements ObjectRepository
+{
+    protected $cache;
+    protected $className;
+    protected $_em;
+
+    public function __construct($em, $className, array $cache = array())
+    {
+        $this->_em          = $em;
+        $this->cache        = $cache;
+        $this->className    = $className;
+    }
+
+    /**
+     * @param mixed $id The identifier.
+     * @return object The object.
+     */
+    public function find($id)
+    {
+        foreach ($this->cache as $object) {
+            if ($object->getId() === $id) {
+                return $object;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @return array
+     */
+    public function findAll()
+    {
+        return $this->cache;
+    }
+
+    /**
+     * Finds objects by a set of criteria.
+     *
+     * Optionally sorting and limiting details can be passed. An implementation may throw
+     * an UnexpectedValueException if certain values of the sorting or limiting details are
+     * not supported.
+     *
+     * @param array $criteria
+     * @param array|null $orderBy
+     * @param int|null $limit
+     * @param int|null $offset
+     * @return array The objects.
+     * @throws \UnexpectedValueException
+     */
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        $objects = array();
+
+        // Find objects which accomplish the conditions
+        foreach ($this->cache as $object) {
+            $satisfiedConditions = 0;
+            foreach ($criteria as $name => $condition) {
+                $getter = $this->generateGetterName($object, $name);
+                if (!isset($getter)) {
+                    continue;
+                }
+
+                $returnedValue = $object->$getter();
+                if ($returnedValue === $condition) {
+                    // Try to find assuming the condition is a property or entity
+                    $satisfiedConditions++;
+                } elseif (is_object($returnedValue)) {
+                    // The condition could be an entityId. Try to cast it to an entity
+                    $condition = $this->getEntityManager()->find(get_class($returnedValue), $condition);
+                    if (isset($condition) && $returnedValue === $condition) {
+                        $satisfiedConditions++;
+                    }
+                }
+            }
+            // If all conditions are satisfied, this object accomplishes the criteria
+            if (count($criteria) == $satisfiedConditions) {
+                $objects[] = $object;
+            }
+        }
+
+        // Order the objects
+        if (isset($orderBy) && count($orderBy) > 0) {
+            $orderBy = $this->castOrderByArray($orderBy);
+            $repo = $this;
+            usort($objects, function ($a, $b) use ($orderBy, $repo) {
+                $pos = 0;
+                while ($pos < count($orderBy)) {
+                    $getterA = $repo->generateGetterName($a, $orderBy[$pos]['fieldName']);
+                    $getterB = $repo->generateGetterName($b, $orderBy[$pos]['fieldName']);
+
+                    if ($orderBy[$pos]['orientation'] == "ASC") {
+                        if ($a->$getterA() > $b->$getterB()) {
+                            return 1;
+                        } elseif ($a->$getterA() < $b->$getterB()) {
+                            return -1;
+                        }
+                    } else {
+                        if ($a->$getterA() > $b->$getterB()) {
+                            return -1;
+                        } elseif ($a->$getterA() < $b->$getterB()) {
+                            return 1;
+                        }
+                    }
+
+                    $pos++;
+                };
+
+                return 0;
+            });
+        }
+
+        // Apply limit and offset
+        if (isset($limit)) {
+            $offset = $offset ?: 0;
+            $objects = array_slice($objects, $offset, $limit);
+        }
+
+        return $objects;
+    }
+
+    /**
+     * Finds a single object by a set of criteria.
+     *
+     * @param array $criteria The criteria.
+     *
+     * @return object The object.
+     */
+    public function findOneBy(array $criteria)
+    {
+        $objects = $this->findBy($criteria);
+        return count($objects) == 0 ? null : $objects[0];
+    }
+
+    /**
+     * @return string
+     */
+    public function getClassName()
+    {
+        return $this->className;
+    }
+
+    /**
+     * @return ObjectManager
+     */
+    public function getEntityManager()
+    {
+        return $this->_em;
+    }
+
+    /**
+     * Generates a getter name from an object and a property name. It checks the getFoo and the isFoo forms.
+     * If none of the methods exist in the object it returns null
+     * @param $object
+     * @param $property
+     * @return null|string
+     */
+    protected function generateGetterName($object, $property)
+    {
+        $getter = 'get' . ucfirst($property);
+        if (!method_exists($object, $getter)) {
+            $getter = 'is' . ucfirst(($property));
+            if (!method_exists($object, $getter)) {
+                return null;
+            }
+        }
+
+        return $getter;
+    }
+
+    /**
+     * Casts an orderBy array from the form of
+     * [$fieldName => $orientation] to [fieldName => $fieldName, orientation => $orientation]
+     * @param array $orderBy
+     * @return array
+     */
+    protected function castOrderByArray(array $orderBy = array())
+    {
+        $result = array();
+
+        foreach ($orderBy as $fieldName => $orientation) {
+            if (is_int($fieldName)) {
+                $fieldName      = $orientation;
+                $orientation    = "ASC";
+            } else {
+                $orientation = strtoupper(trim($orientation));
+            }
+
+            $result[] = array(
+                'fieldName'     => $fieldName,
+                'orientation'   => $orientation
+            );
+        }
+
+        return $result;
+    }
+}
