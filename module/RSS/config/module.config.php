4aa2b4324e322f335558119fcf9c85606d23a3bc
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-09-27 17:51:09 +0200

    Added language files and translator configuration in RSS module

--- module/RSS/config/module.config.php
+++ module/RSS/config/module.config.php
@@ -31,4 +31,14 @@ return array(
         ),
     ),
 
+    'translator' => array(
+        'translation_file_patterns' => array(
+            array(
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ),
+        ),
+    ),
+
 );
